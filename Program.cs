967ae213797f9e0294b92915fb3226114d3d5652
Aamir5102 <121835118+Aamir5102@users.noreply.github.com>
2023-05-01 16:40:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class MathsTutor
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to MathsTutor!");
+
+        Pack pack = new Pack();
+        string projectFolderPath = GetProjectFolderPath();
+        string statisticsFilePath = Path.Combine(projectFolderPath, "statistics.txt");
+        Statistics stats = new Statistics(statisticsFilePath);
+        bool running = true;
+
+
+        while (running)
+        {
+            Tutorial.ShowInstructions();
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Tutorial.ShowInstructions();
+                    Console.WriteLine("Press any key to return to the menu.");
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Tutorial.DealThreeCards(pack, stats);
+                    break;
+                case "3":
+                    Tutorial.DealFiveCards(pack, stats);
+                    break;
+                case "4":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
+            }
+        }
+
+        stats.SaveStats();
+        Console.WriteLine("Goodbye!");
+
+    }
+    public static string GetProjectFolderPath()
+    {
+        string assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        string assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
+        return Directory.GetParent(assemblyDirectory).Parent.Parent.FullName;
+    }
+
+}
---
+++ statistics.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+public class Statistics
+{
+    private string filePath;
+    private int correct;
+    private int incorrect;
+
+    public Statistics(string filePath)
+    {
+        this.filePath = filePath;
+        LoadStats();
+    }
+
+    private void LoadStats()
+    {
+        if (!File.Exists(filePath))
+        {
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                sw.WriteLine("Total,Correct,Incorrect");
+                sw.WriteLine("0,0,0");
+            }
+        }
+
+        using (StreamReader sr = File.OpenText(filePath))
+        {
+            sr.ReadLine(); // Skip header
+            string[] statsLine = sr.ReadLine().Split(',');
+            correct = int.Parse(statsLine[1]);
+            incorrect = int.Parse(statsLine[2]);
+        }
+    }
+
+    public void SaveStats()
+    {
+        string[] lines = { "Total,Correct,Incorrect", $"{correct + incorrect},{correct},{incorrect}" };
+        File.WriteAllLines(filePath, lines);
+    }
+
+    public void IncrementCorrect()
+    {
+        correct++;
+    }
+
+    public void IncrementIncorrect()
+    {
+        incorrect++;
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine("Game Statistics:");
+        Console.WriteLine($"Total games: {correct + incorrect}");
+        Console.WriteLine($"Correct answers: {correct}");
+        Console.WriteLine($"Incorrect answers: {incorrect}");
+    }
+}
